27683f02c46c54442faaf3b3154d4e3aaafae6b9
Shi YuYan <shiyy@cehfs2.com.sg>
2020-07-28 08:51:40 +0800

    added plotly basic graph

---
+++ src/components/TimeSeriesChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="TimeSeries">
+    <plotly :data="data" :layout="layout" :display-mode-bar="false"></plotly>
+  </div>
+</template>
+
+<script>
+  import { Plotly } from 'vue-plotly';
+  
+  export default {
+    data:function(){
+      return{
+        data:[{
+          x: [1,2,3,4],
+          y: [10,15,13,17],
+          type:"scatter"
+        }],
+        layout: {
+          title:'chart title',
+          xaxis: {
+            title: 'xaxis title'
+          },
+          yaxis: {
+            title: 'yaxis title'
+          }
+        }
+        
+      }
+    },
+    components:{
+      Plotly
+    }
+  }
+</script>
--- src/views/EnergyMonitoring.vue
+++ src/views/EnergyMonitoring.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="EnergyMonitoring">
-    <h1>This is this Energy Monitoring page</h1>
+    <!-- <h1>This is this Energy Monitoring page</h1> -->
+      <v-layout row wrap class="mt-4">
+        <v-flex d-flex xs8 offset-xs2>
+          <v-select
+            :items="lines"
+            label="select line"
+            class="ml-4 mr-4"
+          ></v-select>
+          <v-select
+            :items="machines"
+            label="select machine"
+            class="ml-4 mr-4"
+          ></v-select>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <TimeSeries></TimeSeries>
+      </v-layout>
   </div>
 </template>
+
+<script>
+import TimeSeries from '@/components/TimeSeriesChart.vue';
+export default {
+  name: 'EnergyMonitoring',
+  data: function(){
+    return{
+      lines: ['line 1', 'line 2', 'line 3'],
+      machines: ['machine 1', 'machine 2', 'machine 3'],
+    }
+  },
+  components:{
+    TimeSeries,
+  }
+}
+</script>
